f6a013493ab2424e96e86239dfd673db5118fd84
MaxyuEprst <num_one_id@vk.com>
2025-03-03 07:55:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', function () {
     const sections = document.querySelectorAll('.content-container');
     const navLinks = document.querySelectorAll('.navigation a');
+    let currentPos = 0;
 
     window.addEventListener('scroll', () => {
         let current = '';
-
         sections.forEach(section => {
             const sectionTop = section.offsetTop;
             const sectionHeight = section.clientHeight;
@@ -19,5 +19,15 @@ window.addEventListener('load', function () {
                 link.classList.add('active');
             }
         });
+    });
+
+    window.addEventListener('keydown', function(event) {
+        if (event.code === 'Tab') {
+            event.preventDefault();
+            navLinks[currentPos].classList.remove('active');
+            currentPos = (currentPos + 1) % navLinks.length;
+            navLinks[currentPos].classList.add('active');
+            navLinks[currentPos].click();
+        }
     });
 });
